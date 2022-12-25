064d1e7abfd01969bc3d485040875e89d0831fa8
Jon Rafkind <jon@rafkind.com>
2022-12-25 13:38:56 -0800

    move read helper functions

--- lib/core/core.go
+++ lib/core/core.go
@@ -9,7 +9,6 @@ import (
     "strings"
     "bytes"
     "errors"
-    "unicode/utf8"
     "encoding/binary"
     // "encoding/hex"
 )
@@ -145,102 +144,6 @@ func (section *WebAssemblyTypeSection) ConvertToWast(module *WebAssemblyModule, 
 
 func (section *WebAssemblyTypeSection) String() string {
     return "type section"
-}
-
-func ReadU32(reader io.ByteReader) (uint32, error) {
-    var result uint32
-    var shift uint32
-
-    count := 0
-
-    var low byte = 0b1111111
-    var high byte = 1 << 7
-
-    for {
-        next, err := reader.ReadByte()
-        if err != nil {
-            return 0, err
-        }
-
-        use := uint32(next & low)
-
-        result = result | (use << shift)
-        if next & high == 0 {
-            return result, nil
-        }
-
-        shift += 7
-
-        /* Safety check */
-        count += 1
-        if count > 20 {
-            return 0, fmt.Errorf("Read too many bytes in a LEB128 integer")
-        }
-    }
-}
-
-func ReadSignedLEB128(reader io.ByteReader, size int64) (int64, error) {
-    var result int64
-    var shift int64
-
-    count := 0
-
-    var low byte = 0b1111111
-    var high byte = 1 << 7
-
-    for {
-        next, err := reader.ReadByte()
-        if err != nil {
-            return 0, err
-        }
-
-        use := int64(next & low)
-
-        result = result | (use << shift)
-        shift += 7
-
-        if next & high == 0 {
-            if shift < size && next & 0x40 == 0x40 {
-                result = -result
-            }
-
-            return result, nil
-        }
-
-        /* Safety check */
-        count += 1
-        if count > 20 {
-            return 0, fmt.Errorf("Read too many bytes in a LEB128 integer")
-        }
-    }
-
-}
-
-func ReadS32(reader io.ByteReader) (int32, error) {
-    out, err := ReadSignedLEB128(reader, 32)
-    return int32(out), err
-}
-
-func ReadS64(reader io.ByteReader) (int64, error) {
-    return ReadSignedLEB128(reader, 64)
-}
-
-func ReadFloat32(reader io.Reader) (float32, error) {
-    var value float32
-    err := binary.Read(reader, binary.LittleEndian, &value)
-    if err != nil {
-        return 0, err
-    }
-    return value, nil
-}
-
-func ReadFloat64(reader io.Reader) (float64, error) {
-    var value float64
-    err := binary.Read(reader, binary.LittleEndian, &value)
-    if err != nil {
-        return 0, err
-    }
-    return value, nil
 }
 
 /* All integers are encoded using the LEB128 variable-length integer encoding, in either unsigned or signed variant.
@@ -519,43 +422,6 @@ func (section *WebAssemblyImportSection) AddImport(moduleName string, name strin
         Name: name,
         Kind: kind,
     })
-}
-
-func ReadName(reader *ByteReader) (string, error) {
-    length, err := ReadU32(reader)
-    if err != nil {
-        return "", fmt.Errorf("Could not read name length: %v", err)
-    }
-
-    /* FIXME: put limits somewhere */
-    if length > 10 * 1024 * 1024 {
-        return "", fmt.Errorf("Name length too large: %v", length)
-    }
-
-    raw := make([]byte, length)
-    count, err := io.ReadFull(reader, raw)
-    if err != nil {
-        return "", fmt.Errorf("Could not read name bytes %v: %v", length, err)
-    }
-
-    if count != int(length) {
-        return "", fmt.Errorf("Read %v out of %v bytes", count, length)
-    }
-
-    out := ""
-
-    for len(raw) > 0 {
-        next, size := utf8.DecodeRune(raw)
-        if size == 0 {
-            return "", fmt.Errorf("Could not decode utf8 string %v", raw)
-        }
-
-        out = out + string(next)
-
-        raw = raw[size:]
-    }
-
-    return out, nil
 }
 
 type ImportDescription byte
@@ -2601,21 +2467,6 @@ func (section *WebAssemblyDataSection) ConvertToWast(module *WebAssemblyModule, 
 
 func (section *WebAssemblyDataSection) String() string {
     return "data section"
-}
-
-func ReadByteVector(reader *ByteReader) ([]byte, error) {
-    bytes, err := ReadU32(reader)
-    if err != nil {
-        return nil, fmt.Errorf("Could not read vector size: %v", err)
-    }
-
-    vector := make([]byte, bytes)
-    _, err = io.ReadFull(reader, vector)
-    if err != nil {
-        return nil, fmt.Errorf("Could not read %v bytes of vector: %v", bytes, err)
-    }
-
-    return vector, nil
 }
 
 func (module *WebAssemblyFileModule) ReadDataSection(size uint32) (*WebAssemblyDataSection, error) {
---
+++ lib/core/read.go
@@ -0,0 +1,156 @@
+package core
+
+import (
+    "io"
+    "fmt"
+    "unicode/utf8"
+    "encoding/binary"
+)
+
+func ReadU32(reader io.ByteReader) (uint32, error) {
+    var result uint32
+    var shift uint32
+
+    count := 0
+
+    var low byte = 0b1111111
+    var high byte = 1 << 7
+
+    for {
+        next, err := reader.ReadByte()
+        if err != nil {
+            return 0, err
+        }
+
+        use := uint32(next & low)
+
+        result = result | (use << shift)
+        if next & high == 0 {
+            return result, nil
+        }
+
+        shift += 7
+
+        /* Safety check */
+        count += 1
+        if count > 20 {
+            return 0, fmt.Errorf("Read too many bytes in a LEB128 integer")
+        }
+    }
+}
+
+func ReadSignedLEB128(reader io.ByteReader, size int64) (int64, error) {
+    var result int64
+    var shift int64
+
+    count := 0
+
+    var low byte = 0b1111111
+    var high byte = 1 << 7
+
+    for {
+        next, err := reader.ReadByte()
+        if err != nil {
+            return 0, err
+        }
+
+        use := int64(next & low)
+
+        result = result | (use << shift)
+        shift += 7
+
+        if next & high == 0 {
+            if shift < size && next & 0x40 == 0x40 {
+                result = -result
+            }
+
+            return result, nil
+        }
+
+        /* Safety check */
+        count += 1
+        if count > 20 {
+            return 0, fmt.Errorf("Read too many bytes in a LEB128 integer")
+        }
+    }
+
+}
+
+func ReadS32(reader io.ByteReader) (int32, error) {
+    out, err := ReadSignedLEB128(reader, 32)
+    return int32(out), err
+}
+
+func ReadS64(reader io.ByteReader) (int64, error) {
+    return ReadSignedLEB128(reader, 64)
+}
+
+func ReadFloat32(reader io.Reader) (float32, error) {
+    var value float32
+    err := binary.Read(reader, binary.LittleEndian, &value)
+    if err != nil {
+        return 0, err
+    }
+    return value, nil
+}
+
+func ReadFloat64(reader io.Reader) (float64, error) {
+    var value float64
+    err := binary.Read(reader, binary.LittleEndian, &value)
+    if err != nil {
+        return 0, err
+    }
+    return value, nil
+}
+
+func ReadByteVector(reader *ByteReader) ([]byte, error) {
+    bytes, err := ReadU32(reader)
+    if err != nil {
+        return nil, fmt.Errorf("Could not read vector size: %v", err)
+    }
+
+    vector := make([]byte, bytes)
+    _, err = io.ReadFull(reader, vector)
+    if err != nil {
+        return nil, fmt.Errorf("Could not read %v bytes of vector: %v", bytes, err)
+    }
+
+    return vector, nil
+}
+
+func ReadName(reader *ByteReader) (string, error) {
+    length, err := ReadU32(reader)
+    if err != nil {
+        return "", fmt.Errorf("Could not read name length: %v", err)
+    }
+
+    /* FIXME: put limits somewhere */
+    if length > 10 * 1024 * 1024 {
+        return "", fmt.Errorf("Name length too large: %v", length)
+    }
+
+    raw := make([]byte, length)
+    count, err := io.ReadFull(reader, raw)
+    if err != nil {
+        return "", fmt.Errorf("Could not read name bytes %v: %v", length, err)
+    }
+
+    if count != int(length) {
+        return "", fmt.Errorf("Read %v out of %v bytes", count, length)
+    }
+
+    out := ""
+
+    for len(raw) > 0 {
+        next, size := utf8.DecodeRune(raw)
+        if size == 0 {
+            return "", fmt.Errorf("Could not decode utf8 string %v", raw)
+        }
+
+        out = out + string(next)
+
+        raw = raw[size:]
+    }
+
+    return out, nil
+}
